a8f52344af8ddbb7117c5dd71d4984bbe64e4901
Apinop <apinop.soisuwan@gmail.com>
2021-11-21 22:54:48 +0700

    better result! len(s)//2 cos

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,5 +1,5 @@
 def isValid(s):
-    for i in range(len(s)): #done
+    for i in range(len(s)//2): #done
         s = s.replace('()','')
         s = s.replace('[]','')
         s = s.replace('{}','')
@@ -8,8 +8,9 @@ def isValid(s):
             return True
     return False
 
+
 """""
-    result from leetcode :
-    Runtime: 40 ms, faster than 24.95% of Python3 online submissions for Valid Parentheses.
-    Memory Usage: 14.4 MB, less than 7.96% of Python3 online submissions for Valid Parentheses.
+result from leetcode :
+Runtime: 44 ms, faster than 20.22% of Python3 online submissions for Valid Parentheses.
+Memory Usage: 14.2 MB, less than 65.38% of Python3 online submissions for Valid Parentheses.
 """""
